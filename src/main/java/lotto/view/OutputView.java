1fca780e71a482eb5f16f5c7da13ecde3a53ec77
dino.shin <troas96@naver.com>
2024-04-03 16:23:17 +0900

    refactor: 결과 출력 String format 변경

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -13,9 +13,11 @@ public class OutputView {
     private static final String PURCHASE_COUNT_MESSAGE = "개를 구매했습니다.";
     private static final String JOIN_DELIMITER = ", ";
     private static final String GAME_RESULT_ANNOUNCE_MESSAGE = "당첨 통계\n---------";
+    private static final String RANK_RESULT_FORMAT = "%s %d%s%n";
+    private static final String REVENUE_RESULT_FORMAT = "%s%.2f";
     private static final String COUNT_MESSAGE = "개";
-    private static final String FIFTH_MESSAGE = "3개 일 (5000)- ";
-    private static final String FOURTH_MESSAGE = "4개 일 (50000)- ";
+    private static final String FIFTH_MESSAGE = "3개 일치 (5000)- ";
+    private static final String FOURTH_MESSAGE = "4개 일치 (50000)- ";
     private static final String THIRD_MESSAGE = "5개 일치 (1500000원)- ";
     private static final String SECOND_MESSAGE = "5개 일치, 보너스 볼 일치(30000000원) - ";
     private static final String TOTAL_REVENUE_MESSAGE = "총 수익률은 ";
@@ -37,12 +39,12 @@ public class OutputView {
 
     public static void printGameResult2(final WinningStatistics statistics, final RevenueRate revenueRate) {
         System.out.println(GAME_RESULT_ANNOUNCE_MESSAGE);
-        System.out.println(FIFTH_MESSAGE + statistics.findRankCount(FIFTH) + COUNT_MESSAGE);
-        System.out.println(FOURTH_MESSAGE + statistics.findRankCount(FOURTH) + COUNT_MESSAGE);
-        System.out.println(THIRD_MESSAGE + statistics.findRankCount(THIRD) + COUNT_MESSAGE);
-        System.out.println(SECOND_MESSAGE + statistics.findRankCount(SECOND) + COUNT_MESSAGE);
-        System.out.println(FIRST_MESSAGE + statistics.findRankCount(FIRST) + COUNT_MESSAGE);
-        System.out.print(TOTAL_REVENUE_MESSAGE + revenueRate.getValue());
+        System.out.printf(RANK_RESULT_FORMAT, FIFTH_MESSAGE, statistics.findRankCount(FIFTH), COUNT_MESSAGE);
+        System.out.printf(RANK_RESULT_FORMAT, FOURTH_MESSAGE, statistics.findRankCount(FOURTH), COUNT_MESSAGE);
+        System.out.printf(RANK_RESULT_FORMAT, THIRD_MESSAGE, statistics.findRankCount(THIRD), COUNT_MESSAGE);
+        System.out.printf(RANK_RESULT_FORMAT, SECOND_MESSAGE, statistics.findRankCount(SECOND), COUNT_MESSAGE);
+        System.out.printf(RANK_RESULT_FORMAT, FIRST_MESSAGE, statistics.findRankCount(FIRST), COUNT_MESSAGE);
+        System.out.printf(REVENUE_RESULT_FORMAT, TOTAL_REVENUE_MESSAGE, revenueRate.getValue());
         if (revenueRate.isLoss()) {
             System.out.println(RESULT_END_MESSAGE);
         }
