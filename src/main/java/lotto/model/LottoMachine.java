7b243f6ee763bef733da765a64440919bfeeb99f
dino.shin <troas96@naver.com>
2024-04-10 10:47:09 +0900

    chore: 클래스 멤버 순서 변경

--- src/main/java/lotto/model/LottoMachine.java
+++ src/main/java/lotto/model/LottoMachine.java
@@ -10,23 +10,21 @@ public class LottoMachine {
 
     private static final int MIN_LOTTO_NUM = 1;
     private static final int MAX_LOTTO_NUM = 45;
-
     private static final List<LottoBall> LOTTO_NUMBERS = initializeLottoBalls();
+
+    private final LottoNumberSelector randomLottoNumberSelector;
+    private final LottoNumberSelector customLottoNumberSelector;
+
+    public LottoMachine(final LottoNumberSelector randomLottoNumberSelector, final LottoNumberSelector customLottoNumberSelector) {
+        this.randomLottoNumberSelector = randomLottoNumberSelector;
+        this.customLottoNumberSelector = customLottoNumberSelector;
+    }
 
     private static List<LottoBall> initializeLottoBalls() {
         return IntStream.rangeClosed(MIN_LOTTO_NUM, MAX_LOTTO_NUM)
                 .boxed()
                 .map(LottoBall::new)
                 .collect(Collectors.toList());
-    }
-
-    private final LottoNumberSelector randomLottoNumberSelector;
-    private final LottoNumberSelector customLottoNumberSelector;
-
-
-    public LottoMachine(final LottoNumberSelector randomLottoNumberSelector, final LottoNumberSelector customLottoNumberSelector) {
-        this.randomLottoNumberSelector = randomLottoNumberSelector;
-        this.customLottoNumberSelector = customLottoNumberSelector;
     }
 
     public LottoGroup randomGenerate() {
